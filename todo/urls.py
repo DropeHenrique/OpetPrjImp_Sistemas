7cd95f9bfb2bcec3f13520d921e6646a188eddb3
Pedro Crispim <pedrohenriquecrispim@hotmail.com>
2021-03-26 13:24:35 -0300

    Primeiro commit

---
+++ todo/urls.py
@@ -0,0 +1,26 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from app.views import cadastrar_usuario
+from app.views import index
+from app.views import logar_usuario
+
+urlpatterns = [
+    path('logar_usuario', logar_usuario, name="logar_usuario"),
+    path('cadastrar_usuario', cadastrar_usuario, name="cadastrar_usuario"),
+    path('index', index, name="index"),
+]
