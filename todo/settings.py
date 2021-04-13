31d222da0dc6ebc72ff7e3e534b9b15443dffb7a
Pedro Crispim <pedrohenriquecrispim@hotmail.com>
2021-04-13 19:09:13 -0300

    versão 0.3

--- todo/settings.py
+++ todo/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-en'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
